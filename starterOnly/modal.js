74d56f2d0029af896b05907a24eeb1635510fbcc
Morgan <morgand@tutamail.com>
2021-12-01 08:44:55 +0100

    Ajout tests Nom et Prénoms

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,38 +1,90 @@
-/* déclaration variables */
+/********************** déclaration variables **********************/
+let lastNameStatus = false;
+let firstNameStatus = false;
 let radioStatus = false; 
 let checkboxStatus = true;
+let lastName = document.querySelector('#last');
+let firstName = document.querySelector('#first');
+let errorFirstName = document.querySelector('#errFirstName');
+let errorLastName = document.querySelector('#errLastName');
 let errorCheckbox = document.querySelector('#errCheckbox');
 let errorRadios = document.querySelector('#errRadios');
+const arrayRadio = [document.querySelector('#location1'), document.querySelector('#location2'), document.querySelector('#location3'), document.querySelector('#location4'), document.querySelector('#location5'), document.querySelector('#location6')]; //référence des 6 noeuds input radio 
 
-/* ajout de style */
+/********************** ajout de class css **********************/
+errorFirstName.classList.add('errorMessage');
+errorLastName.classList.add('errorMessage');
 errorCheckbox.classList.add('errorMessage');
 errorRadios.classList.add('errorMessage');
 
-/********************** apparition / disparition modal **********************/
-/*référence div englobante formulaire, function display none / block*/
+/********************** déclaration function **********************/
+/* ouverture fermeture modal: référence div form: display none / block */
 function launchModal(){
   document.querySelector('.displayModal').style.display = "block";
 }
 function closeModal(){
   document.querySelector('.displayModal').style.display = "none";
 }
+/* Vérifie noeudEcoute. true si nombre de caractères compris entre 2 et 30: efface message noeudTexte. false: envoie message d'erreur dans noeudTexte*/
+/*
+function checkCaracters(noeudEcoute, noeudTexte){
+  noeudEcoute.addEventListener('change', function(event){
+    let inputData = event.target.value;
+    if ( inputData.length > 1 && inputData.length < 31  ){
+     document.querySelector(noeudTexte).textContent = "";
+     return lastNameStatus = true;
+     }else{
+       document.querySelector(noeudTexte).textContent = "Veuillez entrer dans le champ Nom entre 2 et 30 caractères.";
+       return lastNameStatus = false;
+     }
+    }
+ );
+}
+checkCaracters('lastName', '#errLastName');
+checkCaracters('firstName', '#errFirstName');
+*/
+
+/********************** MAIN **********************/
 /* event clic sur bouton inscription fait apparaitre modal */
 document.querySelector('.btn-signup').addEventListener('click', launchModal);
 /* event clic sur croix fait disparaitre modal*/
 document.querySelector('.close').addEventListener('click', closeModal);
 
-/********************** test radios et checkbox1 cochés **********************/
-/* référence des 6 noeuds input radio */
-const arrayRadio = [document.querySelector('#location1'), document.querySelector('#location2'), document.querySelector('#location3'), document.querySelector('#location4'), document.querySelector('#location5'), document.querySelector('#location6')];
+/************ test input Nom et Prénoms ************/
+lastName.maxlength = 30; /* ? */
+lastName.addEventListener('change', function(event){
+  let inputData = event.target.value;
+  if ( inputData.length > 1 && inputData.length < 31  ){
+   document.querySelector('#errLastName').textContent = "";
+   return lastNameStatus = true;
+   }else{
+     document.querySelector('#errLastName').textContent = "Veuillez entrer dans le champ Nom entre 2 et 30 caractères.";
+     return lastNameStatus = false;
+   }
+  }
+);
+firstName.addEventListener('change', function(event){
+ let firstNameData = event.target.value;
+ if ( firstNameData.length > 1 && firstNameData.length < 31  ){
+  document.querySelector('#errFirstName').textContent = "";
+  return firstNameStatus = true;
+  }else{
+    document.querySelector('#errFirstName').textContent = "Veuillez entrer dans le champ Nom entre 2 et 30 caractères.";
+    return firstNameStatus = false;
+  }
+ }
+);
 
-/* pour chaque noeud du tableau, ajouter un event click, si au moins une radio est cliquée: radioStatus = true */
+/************ test input radios ************/
+/* event click pour chaque noeud du tableau radios, si au moins une radio est cliquée: radioStatus = true */
 arrayRadio.forEach(function(z){
   z.addEventListener('click', function(){
      radioStatus = true;
      errorRadios.textContent = "";
   })
-})
-/* test checkbox1 si cochée: checboxStatus = true sinon false  */
+});
+
+/************ test checkbox1 ************/
 /* référence noeud checbox1 si event click: changement boolean */
 document.querySelector('#checkbox1').addEventListener("click", function(){
  if (checkboxStatus === false){
@@ -43,7 +95,7 @@ document.querySelector('#checkbox1').addEventListener("click", function(){
  }
 });
 
-/********************** Validation du formulaire **********************/
+/************ submit: validation du formulaire ************/
 document.querySelector(".btn-submit").addEventListener('click', function(){
   if( radioStatus === false ){
     errorRadios.textContent = "Vous devez choisir au moins une option pour continuer.";
@@ -51,7 +103,7 @@ document.querySelector(".btn-submit").addEventListener('click', function(){
   if( checkboxStatus === false ){
     errorCheckbox.textContent = "Vous devez accepter les conditions générales pour continuer.";
   }
-  if(checkboxStatus && radioStatus === true ){  
+  if( checkboxStatus && radioStatus === true ){  
     document.querySelector('[name="registration"]').submit();
  }
 });
